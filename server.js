a80984bc169a599911cb11e96adbdd0d74b08172
alfonso46674 <49523948+alfonso46674@users.noreply.github.com>
2020-05-21 22:52:03 -0500

    Add delete functionality to videos.json; Delete videos no longer existing in the playlist

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express()
 const axios = require('axios')
 const fs = require('fs')
 
-const videosDB = require('./videos.json')
 const credentials = require('./credentials')
 
 const PLAYLIST_ID = "PLaV6FKYP2zzHSbavzgd5TmK1dDoLALVIj"
@@ -23,7 +22,7 @@ const getPlayListItems = async playListID => {
     return result.data;
 };
 let counter = 1;
-let temporalVideos = [];
+let temporalVideos = []; // array with the videos from the playlist
 
 getPlayListItems(PLAYLIST_ID).then(data =>{
 
@@ -39,7 +38,7 @@ getPlayListItems(PLAYLIST_ID).then(data =>{
         counter++
     })
 
-
+    // json where the videos are stored with extra metadata
     let dataVideos = JSON.parse(fs.readFileSync('./videos.json'))
 
     //Populate the json with the videos for the first time
@@ -52,8 +51,9 @@ getPlayListItems(PLAYLIST_ID).then(data =>{
     // Checks the new added videos to the playlists and updates the json
     for(let i = 0; i < temporalVideos.length; i++){
         
+        //if there is a video in temporal that is not in the json, then it is added to the json
         if(dataVideos.find(v =>v.videoId == temporalVideos[i].videoId) == undefined){
-            
+
             console.log("Video not included with the id : " + temporalVideos[i].id);
             dataVideos.push({
                 id: dataVideos[dataVideos.length-1].id + 1,
@@ -61,12 +61,24 @@ getPlayListItems(PLAYLIST_ID).then(data =>{
                 downloaded: false
             })
             fs.writeFileSync('./videos.json', JSON.stringify(dataVideos))
+        } 
+    }
+
+    //Deletes videos from the json that have been removed from the temporal
+    for(let i = 0; i < dataVideos.length; i++){
+         //if there is a video in the json that is not in the temporal, then it must be removed from the json
+        if(!(temporalVideos.find(v => v.videoId == dataVideos[i].videoId))){
+            console.log("No longer existing video in the playlist with id: " + dataVideos[i].id);
+
+            //filters out the video that was removed from the temporal playlist
+            dataVideos = dataVideos.filter(v => v.id != dataVideos[i].id)
+
+            fs.writeFileSync('./videos.json', JSON.stringify(dataVideos))
         }
     }
 
-
+    console.log(temporalVideos);
     console.log(dataVideos);
-    // console.log(dataVideos.find(v =>v.videoId == '_SvXQ7k4VXM') != undefined);    
 })
 
 
