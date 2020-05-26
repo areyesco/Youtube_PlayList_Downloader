e0eda14558f0798bbebb997fa2a2b6f368fee47c
alfonso46674 <49523948+alfonso46674@users.noreply.github.com>
2020-05-26 16:41:03 -0500

    First integration of MongoDB actions

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const Song = require('./models/Song')
-const {uuid} = require('uuidv4')
 const port = 4000
-const app = express()
 const axios = require('axios')
+const app = express()
 const fs = require('fs')
 const ytdl = require('ytdl-core')
 const cors = require('cors')
@@ -29,77 +28,80 @@ const getPlayListItems = async playListID => {
     });
     return result.data;
 };
-let counter = 1;
-let temporalVideos = []; // array with the videos from the playlist
 
-getPlayListItems(PLAYLIST_ID).then(data =>{
 
-    data.items.forEach(element =>{
-        //Push every video to the temporalVideos array
-        temporalVideos.push({
-            id: counter,
-            title: element.snippet.title,
+getPlayListItems(PLAYLIST_ID).then( async(data) =>{
+
+    //Add the songs to the DB
+    data.items.forEach(async (element) =>{
+        await Song.createVideo({
             videoId: element.snippet.resourceId.videoId,
+            title: element.snippet.title,
             downloaded: false,
             playlistId: PLAYLIST_ID
         })
-        counter++
+        
     })
 
-    // json where the videos are stored with extra metadata
-    let dataVideos = JSON.parse(fs.readFileSync('./videos.json'))
 
-    //Populate the json with the videos for the first time
-    if(dataVideos.length == 0 || undefined){
-        fs.writeFileSync('./videos.json', JSON.stringify(temporalVideos))
-    }
+
+
+//     // json where the videos are stored with extra metadata
+//     let dataVideos = JSON.parse(fs.readFileSync('./videos.json'))
+
+//     //Populate the json with the videos for the first time
+//     if(dataVideos.length == 0 || undefined){
+//         fs.writeFileSync('./videos.json', JSON.stringify(temporalVideos))
+//     }
 
     
-   else{
-        //update the json with new videos
-    // Checks the new added videos to the playlists and updates the json
-    for(let i = 0; i < temporalVideos.length; i++){
+//    else{
+//         //update the json with new videos
+//     // Checks the new added videos to the playlists and updates the json
+//     for(let i = 0; i < temporalVideos.length; i++){
         
-        //if there is a video in temporal that is not in the json, then it is added to the json
-        if(dataVideos.find(v =>v.videoId == temporalVideos[i].videoId) == undefined){
+//         //if there is a video in temporal that is not in the json, then it is added to the json
+//         if(dataVideos.find(v =>v.videoId == temporalVideos[i].videoId) == undefined){
 
-            console.log("Added video to the json: " + temporalVideos[i].title);
-            dataVideos.push({
-                id: dataVideos[dataVideos.length-1].id + 1,
-                title: temporalVideos[i].title,
-                videoId: temporalVideos[i].videoId,
-                downloaded: false,
-                playListID: PLAYLIST_ID
-            })
-            fs.writeFileSync('./videos.json', JSON.stringify(dataVideos))
-        } 
-    }
+//             console.log("Added video to the json: " + temporalVideos[i].title);
+//             dataVideos.push({
+//                 id: dataVideos[dataVideos.length-1].id + 1,
+//                 title: temporalVideos[i].title,
+//                 videoId: temporalVideos[i].videoId,
+//                 downloaded: false,
+//                 playListID: PLAYLIST_ID
+//             })
+//             fs.writeFileSync('./videos.json', JSON.stringify(dataVideos))
+//         } 
+//     }
 
-    //Deletes videos from the json that have been removed from the temporal
-    for(let i = 0; i < dataVideos.length; i++){
-         //if there is a video in the json that is not in the temporal, then it must be removed from the json
-        if(!(temporalVideos.find(v => v.videoId == dataVideos[i].videoId))){
-            console.log("No longer existing video in the playlist: " + dataVideos[i].title);
+//     //Deletes videos from the json that have been removed from the temporal
+//     for(let i = 0; i < dataVideos.length; i++){
+//          //if there is a video in the json that is not in the temporal, then it must be removed from the json
+//         if(!(temporalVideos.find(v => v.videoId == dataVideos[i].videoId))){
+//             console.log("No longer existing video in the playlist: " + dataVideos[i].title);
 
-            //filters out the video that was removed from the temporal playlist
-            dataVideos = dataVideos.filter(v => v.id != dataVideos[i].id)
+//             //filters out the video that was removed from the temporal playlist
+//             dataVideos = dataVideos.filter(v => v.id != dataVideos[i].id)
 
-            fs.writeFileSync('./videos.json', JSON.stringify(dataVideos))
-        }
-    }
-   }
+//             fs.writeFileSync('./videos.json', JSON.stringify(dataVideos))
+//         }
+//     }
+//    }
 
-    // console.log(temporalVideos);
-    // console.log(dataVideos);
+
+
+
+
 
     //calls function to download videos
-    // downloadVideos()
+   await downloadVideos()
 })
 
 
 
 
-console.log(uuid());
+
 // console.log("Running at port: " + port)
 app.listen(port)
 
@@ -107,7 +109,7 @@ app.listen(port)
 async function downloadVideos(){
     try {
         let urlYoutube = 'https://www.youtube.com/watch?v='
-        let dataVideos = JSON.parse(fs.readFileSync('./videos.json'))
+        let dataVideos = await Song.getAllVideos()
         let title = 'audio'
         let counter = 0;
         let folderDestination = 'C:/Users/Alponcho/Music/Canciones/SoulDisco/'
@@ -149,17 +151,26 @@ async function downloadVideos(){
                     writable.end();
                 })
     
-                writable.on('finish', ()=>{
+                writable.on('finish', async ()=>{
                     console.log("Done downloading: " + dataVideos[i].title);
                     counter++
                     
                     //update the downloaded status of the song in the json
                     dataVideos[i].downloaded = true;
-                    fs.writeFileSync('./videos.json', JSON.stringify(dataVideos))
 
                     //exits process when all of the videos are done being downloaded
                     if(counter == VideosToDownload){
-                        process.exit()
+                        
+                        for(video in dataVideos){ // arreglar********************
+                            console.log(video);
+                            
+                            if(video.downloaded ==  true){
+                                await Song.updateVideo(video.videoId, {downloaded:true})
+                                console.log("updated: " + video.title);
+                            }
+                        }
+
+                        // process.exit()
                     }
                 })
                 //if the song has already been downloaded
